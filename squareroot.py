39b3eb3a7b6f99a2777e9806e176eae3f854bf9c
unknown <olgarozhdestvina@gmail.com>
2020-02-19 20:34:11 +0800

    Newton's method for square roots

--- squareroot.py
+++ squareroot.py
@@ -5,8 +5,11 @@
 def sqrt(x):
     if x < 0:
         print("Please restart the program and enter a POSITIVE NUMBER!")
-    else:
-        print("The square root of {} is approx. {:.1f}".format(x, x ** 0.5))
+    else: # the next three lines taken from https://stackoverflow.com/questions/32291218/use-newtons-method-to-find-square-root-of-a-number
+        ans = -1.0  
+        y = 1.0
+        while abs(y - ans) > 10 **(-10):  
+            ans = y
+            y = y - (y * y - x) / (2 * y)
+        print("The square root of {} is approx. {:.1f}".format(x, y))
 print(sqrt(x = float(input("Please enter a positive number: "))))
-
-# I coudn't manage to make Newton's method for square roots work
